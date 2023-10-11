b2c6cb917df96b33cf60d4666595ccb1bcd53545
Marshall Styczinski <itsmoosh@gmail.com>
2023-10-11 16:37:42 -0700

    Added ignore and clear features for PP use

---
+++ clearPP.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -r Ariel
+rm -r Callisto
+rm -r Dione
+rm -r Enceladus
+rm -r Europa
+rm -r Ganymede
+rm -r Io
+rm -r Mimas
+rm -r Miranda
+rm -r Oberon
+rm -r Pluto
+rm -r Rhea
+rm -r Tethys
+rm -r Titan
+rm -r Titania
+rm -r Triton
+rm -r Umbriel
+
