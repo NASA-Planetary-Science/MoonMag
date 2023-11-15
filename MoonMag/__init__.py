4474d11ee2e68ff625150b86cfda7550f0293dbc
itsmoosh <itsmoosh@users.noreply.github.com>
2023-11-15 02:54:28 +0000

    deploy: fae3d2c55c4cdc506a510dc650c8defd2c3b11d1

--- MoonMag/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-_ROOT = os.path.abspath(os.path.dirname(__file__))
-_excitation = os.path.join(_ROOT, 'excitation')
-_interior = os.path.join(_ROOT, 'interior')
-_induced = os.path.join(_ROOT, 'induced')
--- runMoonMag.py
+++
@@ -1,7 +0,0 @@
-# Run this script to regenerate figures from the manuscript
-# as detailed in the README file.
-
-from MoonMag.run_all import run_all
-
-if __name__ == "__main__":
-    run_all()
